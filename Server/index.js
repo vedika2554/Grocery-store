446895a3fdb7d5b97ef659d45400eccf2cb79efb
vedika2554 <vedikayadav652@gmail.com>
2023-12-05 11:55:16 +0530

    new commit

--- Server/index.js
+++ Server/index.js
@@ -196,7 +196,7 @@ app.get('/orders', async(req, res)=>{
 })
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(_dirname, '..',client, 'build')));
+    app.use(express.static(path.join(_dirname, '..', 'client', 'build')));
 
     app.get('*', (req, res)=>{
         res.sendFile(path.join(_dirname, '..', 'client', 'build', 'index.html'))
