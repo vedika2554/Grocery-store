ee165325a7ec1e013804671ba45f611fb27d5371
vedika2554 <vedikayadav652@gmail.com>
2023-12-07 11:03:29 +0530

    new commit

--- client/src/utils/auth.js
+++
@@ -1,8 +0,0 @@
-export const checkLogin = async ()=>{
-    const user = JSON.parse(localStorage.getItem('user'))|| null;
-
-    if(!user){
-        alert('please login first');
-        window.location.href='/login';
-    }
-}
